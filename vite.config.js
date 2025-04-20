41647585d66bbf45a484039a431e01eb1ca9c303
Mehta <ayushmehta@csus.edu>
2025-04-19 17:06:46 -0700

    URL Updates

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
+  console.log(env.SOME_KEY)
   return {
     define: {
       'process.env.SOME_KEY': JSON.stringify(env.SOME_KEY)
     },
     plugins: [react()],
+    preview: {
+      allowedHosts: ['ipge-chatbot.azurewebsites.net', 'csusipge.com  ']
+    }
   }
 })
